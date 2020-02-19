a92c9aebef725dc137a25fa7f35c49e1ff212b83
Beier22 <43845116+Beier22@users.noreply.github.com>
2020-02-19 12:02:04 +0100

    Add and Remove Button

--- app/src/main/java/com/example/c1dice/MainActivity.java
+++ app/src/main/java/com/example/c1dice/MainActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView die;
+    LinearLayout layout;
+    LinearLayout.LayoutParams params;
+    int dieCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         die = findViewById(R.id.dice);
         Button btn = findViewById(R.id.btn);
+        Button addBtn = findViewById(R.id.addBtn);
+        Button remBtn = findViewById(R.id.remBtn);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,24 +35,46 @@ public class MainActivity extends AppCompatActivity {
                 RollDice();
             }
         });
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddDie();
+            }
+        });
+        remBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RemoveDie();
+            }
+        });
+
+        layout = findViewById(R.id.layout);
+        params = new LinearLayout.LayoutParams(
+                0,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.weight = 1;
+        dieCount = 1;
     }
 
     private void RollDice(){
         SetEyes(die);
-        /*int rand = new Random().nextInt(6) + 1;
-        if (rand == 1){
-            die.setImageResource(R.drawable.dice1);
-        } else if (rand == 2){
-            die.setImageResource(R.drawable.dice2);
-        } else if (rand == 3){
-            die.setImageResource(R.drawable.dice3);
-        } else if (rand == 4){
-            die.setImageResource(R.drawable.dice4);
-        } else if (rand == 5){
-            die.setImageResource(R.drawable.dice5);
-        } else if (rand == 6){
-            die.setImageResource(R.drawable.dice6);
-        }*/
+    }
+
+    private void AddDie() {
+        if(dieCount < 6){
+            ImageView temp = new ImageView(this);
+            temp.setImageResource(R.drawable.dice1);
+            layout.addView(temp, dieCount, params);
+            dieCount++;
+        }
+    }
+
+    private void RemoveDie() {
+        if(dieCount > 1){
+            layout.removeViewAt(dieCount - 1);
+            dieCount--;
+        }
     }
 
     private void SetEyes(ImageView die){
@@ -70,5 +99,11 @@ public class MainActivity extends AppCompatActivity {
                 die.setImageResource(R.drawable.dice6);
                 break;
         }
+    }
+
+    private int InPixels(int dp) {
+        final float scale = this.getResources().getDisplayMetrics().density;
+        int pixels = (int) (dp * scale + 0.5f);
+        return pixels;
     }
 }
